6d1469c8d7636fcf61028dc155f13d7ec299275d
dcruz354 <dcruz354@gmail.com>
2022-02-12 09:38:07 -0800

    Hello World

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+        <Text>This is my Week 5 airbnb react native application.</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor:  'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  }
 });
